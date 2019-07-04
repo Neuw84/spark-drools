c3a036db766dcad7e1c0a550b1f53444fda75c18
Conde Angel <aconde@ikerlan.es>
2019-07-04 15:00:52 +0200

    updated to latest version of spark

--- src/main/java/com/redhat/gpte/App.java
+++ src/main/java/com/redhat/gpte/App.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.redhat.gpte;
 
@@ -9,7 +9,6 @@ import java.util.Arrays;
  * @author prakrish
  *
  */
-
 import java.util.List;
 
 import org.apache.spark.SparkConf;
@@ -20,55 +19,75 @@ import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.StatelessKieSession;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
 
 public class App {
-  public static void main(String[] args) {
-	
-	 //Input the Data  
-    List<Applicant> inputData = Arrays.asList(
-      new Applicant(1, "Jerry", "Seinfeld", 10000, 568),
-      new Applicant(2, "Michael", "Richards", 12000, 654),
-      new Applicant(3, "Cosmo", "Kramer", 100, 568),
-      new Applicant(4, "Elaine", "Benes", 1000000, 788),
-      new Applicant(5, "Susan", "Ross", 10, 788),
-      new Applicant(6, "Jake", "Harper", 34000, 900),
-      new Applicant(7, "John", "Cryer", 12000, 457),
-      new Applicant(8, "Walden","Schimdt", 10000, 300),
-      new Applicant(9,  "Julie", "Dreyfus",20000, 721),
-      new Applicant(10, "Jason", "Alexandar", 25000, 590)
-    );
-    //Default 4 threads will be invoked, if it set to local[*], local[2] 2 threads
-    SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local[*]");
-    JavaSparkContext sc = new JavaSparkContext(conf);
 
-   
-    KieBase rules = loadRules();
-    Broadcast<KieBase> broadcastRules = sc.broadcast(rules);
+    public static void main(String[] args) {
 
-    //Spark used to parallelize the input data and fire the rules for the rule which is in com/redhat/approval.drl
-    JavaRDD<Applicant> applicants = sc.parallelize(inputData);
+        //Input the Data  
+        List<Applicant> inputData = Arrays.asList(
+                new Applicant(1, "Jerry", "Seinfeld", 10000, 568),
+                new Applicant(2, "Michael", "Richards", 12000, 654),
+                new Applicant(3, "Cosmo", "Kramer", 100, 568),
+                new Applicant(4, "Elaine", "Benes", 1000000, 788),
+                new Applicant(5, "Susan", "Ross", 10, 788),
+                new Applicant(6, "Jake", "Harper", 34000, 900),
+                new Applicant(7, "John", "Cryer", 12000, 457),
+                new Applicant(8, "Walden", "Schimdt", 10000, 300),
+                new Applicant(9, "Julie", "Dreyfus", 20000, 721),
+                new Applicant(10, "Jason", "Alexandar", 25000, 590)
+        );
 
-    long numApproved = applicants.map( a -> applyRules(broadcastRules.value(), a) )
-                                 .filter( a -> a.isApproved() )
-                                 .count();  //First Action Performed on Spark
+        SparkConf conf = new SparkConf();
+        //as recomended by databricks
+        conf.set("spark.hadoop.fs.s3a.multipart.threshold", "2097152000");
+        conf.set("spark.hadoop.fs.s3a.multipart.size", "104857600");
+        conf.set("spark.hadoop.fs.s3a.connection.maximum", "500");
+        conf.set("spark.hadoop.fs.s3a.connection.timeout", "600000");
+        //Spark session is the main entry point for interfacing with it
+        //remember that spark session is 'safe' and can be shared among threads
 
-    System.out.println("Number of applicants approved: " + numApproved);
-    applicants.saveAsTextFile("com.redhat.gpte"); // 2nd Action is Performed on this Job to Save the File.
-    sc.close();
-  }
-  
+        SparkSession spark = SparkSession.builder()
+                .appName("Simple Application")
+                .config(conf)
+                .master("local[*]")
+                .getOrCreate();
+
+        JavaSparkContext jsc = JavaSparkContext.fromSparkContext(spark.sparkContext());
+        KieBase rules = loadRules();
+        Broadcast<KieBase> broadcastRules = jsc.broadcast(rules);
+
+        //Spark used to parallelize the input data and fire the rules for the rule which is in com/redhat/approval.drl
+        Dataset<Applicant> applicantsDS = spark.createDataset(inputData, Encoders.bean(Applicant.class));
+        applicantsDS.show(false);
+
+
+        applicantsDS
+                .map(a -> applyRules(broadcastRules.value(), a), Encoders.bean(Applicant.class))
+                .filter(a -> a.isApproved());
+        
+        applicantsDS.show(false);
+        //       .count();  //First Action Performed on Spark
+
+//        System.out.println("Number of applicants approved: " + numApproved);
+        //applicants.saveAsTextFile("com.redhat.gpte"); // 2nd Action is Performed on this Job to Save the File.
+        spark.close();
+    }
+
 //Load the Rules (approval.drl) in the container 
-  public static KieBase loadRules() {
-    KieServices kieServices = KieServices.Factory.get();
-    KieContainer kieContainer = kieServices.getKieClasspathContainer();
-    return kieContainer.getKieBase();
-  }
-  
-	
- // Fire/apply the rules if the credit score > 600
-  public static Applicant applyRules(KieBase base, Applicant a) {
-    StatelessKieSession session = base.newStatelessKieSession();
-    session.execute(a);
-    return a;
-  }
+    public static KieBase loadRules() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        return kieContainer.getKieBase();
+    }
+
+    // Fire/apply the rules if the credit score > 600
+    public static Applicant applyRules(KieBase base, Applicant a) {
+        StatelessKieSession session = base.newStatelessKieSession();
+        session.execute(a);
+        return a;
+    }
 }
--- src/main/java/com/redhat/gpte/Applicant.java
+++ src/main/java/com/redhat/gpte/Applicant.java
@@ -1,35 +1,88 @@
 package com.redhat.gpte;
 
-
 public class Applicant implements java.io.Serializable {
-  /**
-	 * 
-	 */
-	private static final long serialVersionUID = -7080955793548250917L;
-private String firstName;
-  private String lastName;
 
-  private int id;
-  private int requestAmount;
-  private int creditScore;
+    /**
+     *
+     */
+    private static final long serialVersionUID = -7080955793548250917L;
+    private String firstName;
+    private String lastName;
 
-  private boolean approved;
+    private int id;
+    private int requestAmount;
+    private int creditScore;
 
-  public Applicant(int _id, String _firstName, String _lastName, int _requestAmount, int _creditScore) {
-    id = _id;
-    firstName = _firstName;
-    lastName = _lastName;
-    requestAmount = _requestAmount;
-    creditScore = _creditScore;
-  }
+    private boolean approved;
 
-  public String getFirstName() { return firstName; }
-  public String getLastName() { return lastName; }
+    public Applicant(int _id, String _firstName, String _lastName, int _requestAmount, int _creditScore) {
+        id = _id;
+        firstName = _firstName;
+        lastName = _lastName;
+        requestAmount = _requestAmount;
+        creditScore = _creditScore;
+    }
 
-  public int getId() { return id; }
-  public int getRequestAmount() { return requestAmount; }
-  public int getCreditScore() { return creditScore; }
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-  public boolean isApproved() { return approved; }
-  public void setApproved(boolean _approved) { approved = _approved; }
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @param requestAmount the requestAmount to set
+     */
+    public void setRequestAmount(int requestAmount) {
+        this.requestAmount = requestAmount;
+    }
+
+    /**
+     * @param creditScore the creditScore to set
+     */
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRequestAmount() {
+        return requestAmount;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean _approved) {
+        approved = _approved;
+    }
 }
